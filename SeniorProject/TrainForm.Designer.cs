acfa1d058f0a7fc80a7041a1df85fa157d2de309
JuJiiz <ju.jiiz.1579@gmail.com>
2018-02-08 22:55:48 +0700

    Add project files.

---
+++ SeniorProject/TrainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SeniorProject
+{
+    partial class TrainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVideoBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFileLocal = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbExample = new System.Windows.Forms.PictureBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExample)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVideoBrowse
+            // 
+            this.btnVideoBrowse.Location = new System.Drawing.Point(12, 250);
+            this.btnVideoBrowse.Name = "btnVideoBrowse";
+            this.btnVideoBrowse.Size = new System.Drawing.Size(120, 29);
+            this.btnVideoBrowse.TabIndex = 0;
+            this.btnVideoBrowse.Text = "Browse Video";
+            this.btnVideoBrowse.UseVisualStyleBackColor = true;
+            this.btnVideoBrowse.Click += new System.EventHandler(this.browse_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Local: ";
+            // 
+            // lbFileLocal
+            // 
+            this.lbFileLocal.AutoSize = true;
+            this.lbFileLocal.Location = new System.Drawing.Point(67, 9);
+            this.lbFileLocal.Name = "lbFileLocal";
+            this.lbFileLocal.Size = new System.Drawing.Size(22, 13);
+            this.lbFileLocal.TabIndex = 2;
+            this.lbFileLocal.Text = "xxx";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbFileLocal);
+            this.panel1.Location = new System.Drawing.Point(138, 250);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 29);
+            this.panel1.TabIndex = 4;
+            // 
+            // pbExample
+            // 
+            this.pbExample.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pbExample.Location = new System.Drawing.Point(12, 12);
+            this.pbExample.Name = "pbExample";
+            this.pbExample.Size = new System.Drawing.Size(477, 222);
+            this.pbExample.TabIndex = 10;
+            this.pbExample.TabStop = false;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(413, 285);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 58);
+            this.btnRun.TabIndex = 11;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.run_click);
+            // 
+            // TrainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 355);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.pbExample);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnVideoBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "TrainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExample)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVideoBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbFileLocal;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbExample;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
---
+++ SeniorProject/TrainForm.cs
@@ -0,0 +1,95 @@
+﻿using Accord.Imaging.Filters;
+using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeniorProject
+{
+    public partial class TrainForm : Form
+    {
+        private String VideoInputPath = "";
+        private String VideoOutputPath = "";
+        public TrainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.lbFileLocal.Text = open.FileName;
+                VideoInputPath = open.FileName;
+            }
+        }
+
+        private void run_click(object sender, EventArgs e)
+        {
+            if (!lbFileLocal.Text.Equals("xxx"))
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        string[] files = Directory.GetFiles(fbd.SelectedPath);
+                        VideoOutputPath = fbd.SelectedPath;
+                        //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+                        VideoFileReader read = new VideoFileReader();
+                        read.Open(VideoInputPath);
+                        Grayscale f1 = new Grayscale(0.2125, 0.7154, 0.0721);
+                        HistogramEqualization f3 = new HistogramEqualization();
+                        CannyEdgeDetector fiw = new CannyEdgeDetector(0, 5);
+                        OtsuThreshold f4 = new OtsuThreshold();
+                        HorizontalRunLengthSmoothing f6 = new HorizontalRunLengthSmoothing(10);
+                        VerticalRunLengthSmoothing f7 = new VerticalRunLengthSmoothing(30);
+                        GaussianBlur fiii = new GaussianBlur();
+                        HSLFiltering filter = new HSLFiltering();
+                        filter.Luminance = new Accord.Range(0.95f, 1);
+                        for (int i = 0; i < read.FrameCount; i++)
+                        {
+                            Bitmap image = read.ReadVideoFrame();
+                            Bitmap box = new Bitmap(image, 400, 300);
+                            Bitmap w1 = f3.Apply(box);
+                            Bitmap w2 = filter.Apply(w1);
+                            Bitmap w3 = f1.Apply(w2);
+                            Bitmap ax5 = fiii.Apply(w3);
+                            Bitmap ax6 = fiw.Apply(ax5);
+                            Bitmap w4 = f4.Apply(ax5);
+                            Bitmap ax7 = fiw.Apply(w4);
+
+                            ax6.Save(VideoOutputPath + "/normal/" + i + ".jpg" , ImageFormat.Jpeg);
+                            ax7.Save(VideoOutputPath + "/otsu/" + i + ".jpg", ImageFormat.Jpeg);
+                            ax5.Dispose();
+                            box.Dispose();
+                            image.Dispose();
+                            ax6.Dispose();
+                            ax7.Dispose();
+                            w1.Dispose();
+                            w2.Dispose();
+                            w3.Dispose();
+                            w4.Dispose();
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+            }
+            
+        }
+    }
+}
